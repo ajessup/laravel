25d635168c716dcd67665fd28677fa24ec2efca6
Andrew Jessup <jessup@andrew1.roam.corp.google.com>
2013-12-26 22:11:58 +1100

    Clean up monolog instanciation

--- app/start/global.php
+++ app/start/global.php
@@ -31,11 +31,10 @@ ClassLoader::addDirectories(array(
 |
 */
 
-use Monolog\Logger;
 $monolog = Log::getMonolog();
 
 $monolog->pushHandler(
-	new Monolog\Handler\SyslogHandler('intranet', 'user', Logger::DEBUG, false, LOG_PID)
+	new Monolog\Handler\SyslogHandler('intranet', 'user')
 );
 
 /*
